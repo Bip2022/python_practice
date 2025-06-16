3dde703f061404a49132ae7cafb5cd6431cd0c2c
Bipin Ghimire <beepinghimire2022@gmail.com>
2025-06-16 21:35:49 +0545

    variable and list and its case style understand

---
+++ day1.py
@@ -0,0 +1,27 @@
+first_name ="Bipin"
+age = 20
+
+print(first_name)
+print(age+2)
+print ("my first_name is",first_name)
+print ("my first_name is "+ first_name)
+print(f"My first_name is {first_name}")
+print("My first_name is {}".format(first_name))
+
+# not recommended(variable)
+# friend1 = "Ram"
+# friend2 = "Hari"
+# friend3 = "Rohit"
+# print(friend1, friend2, friend3)
+
+# recommended(list)
+friends=['ram','hari','shyam',1,True]
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[4-1])  # Accessing the last element using negative indexing
+print(friends[4-2])
+
+
