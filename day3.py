64484165705525dd92c136a622759c6a8471216b
Bipin Ghimire <beepinghimire2022@gmail.com>
2025-06-18 21:08:30 +0545

    conditional

---
+++ day3.py
@@ -0,0 +1,56 @@
+# if-statement
+# age = 18
+# if age >= 18:
+#     print("You are an adult.")
+
+# if-else statement
+# age = 16
+# if age >= 18:
+#     print("You are a adult.")
+# else:
+#     print("You are an adult.")
+
+# if-elif-else statement
+# marks = 65
+
+# if marks >= 80:
+#     print("A grade")
+# elif marks >= 60:
+#     print("B grade")
+# elif marks >= 40:
+#     print("C grade")
+# else:
+#     print("Fail")
+
+# Nested if statement
+# age = 22
+# has_id = True
+
+# if age >= 18:
+#     if has_id:
+#         print("Accepted")
+#     else:
+#         print("Prohibited")
+# else:
+#     print("Underage,prohibited")
+
+# one-line if statement
+# age=17
+# status ="Adult " if age >= 18 else "Minor"
+# print(status)
+
+# x=10
+# y=5
+# if x > y:
+#     print("x is greater than y")
+# elif x < y:
+#     print("x is less than y")
+# else:
+#     print("x is equal to y")
+
+# x=10
+# y=-2
+# if x>0 and y>0:
+#     print("Both x and y are positive")
+# else:
+#     print("At least one of x or y is not positive")
